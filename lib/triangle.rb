7d9dac84df3d656d6fc55244e1a430e349a61cde
fusion407 <fusion407@gmail.com>
2023-02-01 12:34:27 -0600

    commit tree 15e5f63fffc717dd30842ef4fcde9727c8f7f730

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  attr_reader :equilateral, :isosceles, :scalene
+
+  def initialize(a, b, c)
+    @@a = a
+    @@b = b
+    @@c = c
+  end
+  def kind
+    if @@a == 0 and @@b == 0 and @@c == 0
+      raise TriangleError
+    elsif @@a < 0 or @@b < 0 or @@c < 0
+      raise TriangleError
+    elsif @@a + @@b <= @@c or @@a + @@c <= @@b or @@b + @@c <= @@a
+      raise TriangleError
+    end
+    if @@a == @@b and @@a == @@c
+      return :equilateral
+    elsif @@a == @@b or @@a == @@c or @@b == @@c
+      return :isosceles
+    elsif @@a != @b and @@a != @@c and @@b != @@c
+      return :scalene
+    end
+  end
+  class TriangleError < StandardError
+    def message
+      "triangle error, there are no sides"
+    end
+  end
 end
